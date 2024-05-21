07f9420ce9f30d3487c82202d5ef5bfbd8c961af
Axel Schneider <axel99092@t-online.de>
2024-05-21 18:15:02 +0200

    Update Gtk4VideoPlayer.py
    added Shortcut - H to toggle headerbar visible
    added Button to toggle Dark / Light theme

--- Gtk4VideoPlayer.py
+++ Gtk4VideoPlayer.py
@@ -10,10 +10,11 @@ from gi.repository import Gtk, Gdk, Adw, Gio, GLib
 from subprocess import check_output, STDOUT
 
 CSS = """
-#myheaderbar, #myappwindow {
-    background: black;
-    color: lightgray;
-    border-radius: 12px 12px 4px 4px;
+.title {
+    font-size: 11pt;
+}
+#myplayer {
+    background: #000;
 }
 """
 
@@ -21,7 +22,10 @@ CSS = """
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(title="Video Player", *args, **kwargs)
-
+        
+        self.use_dark_theme = True
+        sm.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+        
         self.set_name("myappwindow")
         self.set_icon_name("multimedia-video-player")
         self.current_folder = Gio.File.new_for_path(
@@ -44,15 +48,20 @@ class MainWindow(Gtk.ApplicationWindow):
         self.header_bar.set_name("myheaderbar")
         self.set_titlebar(titlebar=self.header_bar)
         
-        btn_url = Gtk.Button.new_from_icon_name("edit-paste")
+        btn_url = Gtk.Button.new_from_icon_name("edit-paste-symbolic")
         btn_url.set_tooltip_text("Open URL from clipboard\nFile or Web or Youtube")
         btn_url.connect("clicked", self.open_url)
         self.header_bar.pack_end(btn_url)
 
-        btn_open = Gtk.Button.new_from_icon_name("document-open")
+        btn_open = Gtk.Button.new_from_icon_name("document-open-symbolic")
         btn_open.set_tooltip_text("Open File")
         btn_open.connect("clicked", self.open_file)
         self.header_bar.pack_end(btn_open)
+        
+        btn_theme = Gtk.Button.new_from_icon_name("preferences-desktop-theme-symbolic")
+        btn_theme.set_tooltip_text("toggle theme\nDark / Light")
+        btn_theme.connect("clicked", self.toggle_theme)
+        self.header_bar.pack_end(btn_theme)
 
         self.video_file = ""
         self.player = Gtk.Video.new()
@@ -79,10 +88,19 @@ class MainWindow(Gtk.ApplicationWindow):
             "video/webm",
             "video/mpeg",
             "video/x-msvideo",
-            "video/3gpp",
+            "video/3gpp", 
+            "video/ogg"
         ]
         for vid in mime_types:
             self.file_filter_videos.add_mime_type(vid)
+            
+    def toggle_theme(self, *args):
+        if self.use_dark_theme == False:
+            sm.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+            self.use_dark_theme = True
+        else:
+            sm.set_color_scheme(Adw.ColorScheme.FORCE_LIGHT)
+            self.use_dark_theme = False
 
     def open_file(self, *args):
         print("open file")
@@ -95,21 +113,20 @@ class MainWindow(Gtk.ApplicationWindow):
         url = self.clipboard.read_text_finish(result)
         if url is not None:
             if url.startswith("http"):
-                if "youtube" in url[:22] or "youtu.be" in url[:22]:
+                if ("youtube" in url[:22] or "youtu.be" in url[:22] 
+                    or "mediathek" in url[:25] or "zdf.de" in url[:25]):
                     print("is youtube, grabbing video url")
-                    url = self.get_yt_url(url)
+                    url = self.get_yt_url(url).strip()
                     self.set_title("Youtube Video")
+                    print(f"grabbed: {url}")
                 else:
-                    url = url
                     self.set_title("Web Video")
-                print(url)
-                self.video_file = str(url)
-                self.player.set_file(Gio.File.new_for_uri(self.video_file))               
+                    print(url)
+                self.player.set_file(Gio.File.new_for_uri(url))
             else:
                 print(url)
-                self.video_file = str(url)
-                self.player.set_file(Gio.File.new_for_path(self.video_file))
-                name = self.video_file.split("/")[-1].split(".")[-2]
+                self.player.set_file(Gio.File.new_for_path(url))
+                name = url.split("/")[-1].split(".")[-2]
                 self.set_title(name)
                 
     def on_paste_url_startup(self, url, *args):
@@ -119,11 +136,12 @@ class MainWindow(Gtk.ApplicationWindow):
                     print("is youtube, grabbing video url")
                     url = self.get_yt_url(url)
                     self.set_title("Youtube Video")
+                    print(f"grabbed: {url}")
                 else:
                     url = url
                     self.set_title("Web Video")
-                print(url)
-                self.video_file = str(url)
+                    print(url)
+                self.video_file = url
                 self.player.set_file(Gio.File.new_for_uri(self.video_file))               
             else:
                 print(url)
@@ -152,9 +170,11 @@ class MainWindow(Gtk.ApplicationWindow):
         dialog.destroy()
         
     def get_yt_url(self, url, *args):
-        cmd = f"yt-dlp -g -f worst {url}"
-        result = check_output(cmd, stderr=STDOUT, shell=True).decode()
-        result = f'http{result.split("http")[1]}'
+        if "mediathek" in url or "zdf.de" in url:
+            cmd = f"yt-dlp --no-warnings -g -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' {url}"
+        else:
+            cmd = f"yt-dlp --no-warnings -g -f 'worst/worstvideo+bestaudio' {url}"
+        result = check_output(cmd, stderr=STDOUT, shell=True).decode().split("\n")[0]
         return result
 
 
@@ -182,15 +202,26 @@ class MyApp(Adw.Application):
                 self.win.set_cursor(cursor_blank)
             else:
                 self.win.set_cursor(cursor_def)
+        if keycode == ord('h'):
+            self.toggle_headerbar()
                 
     def on_activate(self, app, *args, **kwargs):
         self.win = MainWindow(application=app)
         self.win.present()
         keycont = Gtk.EventControllerKey()
         keycont.connect("key-pressed", self.lol, self.win)
-        self.win.add_controller(keycont)      
+        self.win.add_controller(keycont)   
+     
+    def toggle_headerbar(self):
+        allocation = self.win.get_allocation()
+        if self.win.header_bar.is_visible():
+            self.win.header_bar.set_visible(False)
+            self.win.set_default_size(allocation.width, allocation.height - 44)
+        else:
+            self.win.header_bar.set_visible(True)
+            self.win.set_default_size(allocation.width, allocation.height + 44)
 
 
 app = MyApp()
+sm = app.get_style_manager()
 app.run(sys.argv)
-
